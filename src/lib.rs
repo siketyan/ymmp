ba5aa0fd003b17dc49559a9dfe2e4b7e7d1f889d
Naoki Ikeguchi (YUMEMI Inc.) <n_ikeguchi@yumemi.co.jp>
2021-07-08 18:20:31 +0900

    ✨ Introducing YmmpSocket, which supports both receiving and sending via a UDP socket

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod broadcaster;
 mod header;
 mod packet;
 mod receiver;
+mod socket;
 
 pub(crate) type Octet = u8;
 pub(crate) type Octets = [Octet];
@@ -10,3 +11,4 @@ pub use crate::broadcaster::Broadcaster;
 pub use crate::header::Header;
 pub use crate::packet::Packet;
 pub use crate::receiver::Receiver;
+pub use crate::socket::YmmpSocket;
